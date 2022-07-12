ea8d77fc66d89b38a141add894cab9b67abd88f7
Leslio McKeown <45811069+LeslioMcKeown@users.noreply.github.com>
2022-07-12 23:39:41 +0000

    Initial commit

---
+++ .lesson/instructions.md
@@ -0,0 +1,40 @@
+# Instructions  
+
+Today you will create a dictionary and a list.
+
+For the dictionary, you will create key:value pairs of food prices.
+For the list, you will create a list of your playlist.
+
+## Steps
+No.| To do
+:-:|:-
+1| In your "main.py" create a dictionary of a food table with the following key:value pairs: Chicken = 1.59, Beef = 1.99, Cheese = 1.00, & Milk = 2.50. Put these in lines 7-12, making sure to input the keys as strings & to add a comma in between each pair.
+2| Access the price for each item and store into their own variable in lines 22-24. Use the milk example in line 18 to model your answer.
+3| Print the prices for the 4 items in line 27.
+4| Now create playlist, which is a list of the songs in your section's Spotify playlist. Add the songs as strings in line 33.
+5| Print the playlist to see all the songs that you added.
+6| Index the first song in your playlist in line 41.
+7| The 2nd & 3rd songs are your favorite in this playlist. Access them & create a variable called "fave_songs" in line 44. Then print your subset in line 45.
+8| Update your playlist by changing the last song to something else. Write this song in line 48. Then print this playlist.
+9| Add another song at the end of the list using append() in line 53. Then, print the updated playlist.
+10| You changed your mind & want to delete the last song you just added using pop() in line 58. Now print your playlist again.
+11| How many songs are in your playlist? Run line 63 & type the answer in line 64.
+12| Sort your playlist in alphabetical order using sorted() in line 67. Finally type the songs in the sorted order based on Step 11.
+
+
+  Use [Markdown](https://gist.github.com/cuonggt/9b7d08a597b167299f0d) to format your instructions.
+
+  For example, here is a code block in python3
+```python
+def hello_world():
+  print("hello world!")
+```
+
+
+  Include an image by placing it in the `assets` folder.
+
+  For example, here is the Replit logo:
+
+  ![alt text](assets/logo.png)
+  
+  
---
+++ main.py
@@ -0,0 +1,69 @@
+"""" EXIT TICKET """
+# Rename this file here & call it week4_exit.py
+
+## Part I: Create a dictionary
+
+# Create your food table here via key:value pairs
+# (input the food & prices from lines 73-76):
+food_prices = {
+
+
+
+  
+  }
+print(food_prices)
+
+# Retrieve any value from the food prices dictionary by using its corresponding key along with the bracket notation
+# For example, to get the price for cheese, run this:
+milk_price = food_prices["milk"]
+print(milk_price)
+
+# Access the prices for each item & store into their own variable, just like the milk example.  
+chicken_price =
+beef_price =
+cheese_price
+
+# Print those variables to check that you get the right value. Write the code to print on the line below:
+print(    )
+
+## Part II. Create a list of the Spotify playlist
+
+# First, add the songs in your section's list in 
+playlist = [
+  
+]
+
+# Print the playlist in the line below:
+print(playlist)
+
+# Lists have an index, which is a number assigned to each item in the list based on the oder in which they appear in the list, starting at 0. 
+# What's the first song on your list?
+print(playlist[0])
+
+# Pick your top 2 favorite songs from the list & print them using the list's index to access them
+fave_songs = playlist[1:2]
+print(fave_songs)
+
+# Update your playlist by changing the last song to something else
+playlist[2] = " "
+# print the playlist to show that you have updated it
+print(playlist)
+
+# Add another song to the end of the list using append()
+playlist.append(" ")
+# print the playlist to show that you added a new song
+print(playlist)
+
+# You changed your mind & want to delete the last song you just added
+playlist.pop()
+# Print the playlist to show that you deleted the last song
+print( )
+
+# How many songs are in your list? Use len()
+print(len(playlist))
+# Write here how many songs are in your playlist = 
+
+# Sort your playlist in alphabetical order using sorted()
+print(sorted(playlist))
+# Write here the order of the songs in your playlist:
+# 
